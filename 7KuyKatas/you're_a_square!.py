12df1500d51edf8d952f098a24f66d4407d08848
Ireal <pesotskygoscha@yandex.ru>
2022-11-15 20:02:51 +0300

    added documentation

--- 7KuyKatas/you're_a_square!.py
+++ 7KuyKatas/you're_a_square!.py
@@ -1,9 +1,14 @@
-def is_square(n):
+def is_square(n: int) -> bool:
+    """
+    This function checks if the given number is the square of another number.
+    :param n: number to check
+    :return: true if the number is the square of the number. false if the number is not a square
+    """
     if n < 0:
         return False
     else:
         for i in range(n + 1):
-            i2 = i * i
+            i2: int = i * i
             if i2 == n:
                 return True
             elif i2 > n:
