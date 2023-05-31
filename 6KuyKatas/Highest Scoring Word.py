159b44d89c86c11d5ff308477cf18d674091d8e8
Ireal <pesotskygoscha@yandex.ru>
2023-05-31 12:12:59 +0300

    solved task Highest Scoring Word

---
+++ 6KuyKatas/Highest Scoring Word.py
@@ -0,0 +1,17 @@
+def high(x):
+    chars_and_raitings = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8,
+                          'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
+                          'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23,
+                          'x': 24, 'y': 25, 'z': 26}
+    words = x.split()
+    result_word = ''
+    raiting_result_word = 0
+
+    for word in words:
+        word_raiting = 0
+        for char in word:
+            word_raiting += chars_and_raitings[char]
+        if word_raiting > raiting_result_word:
+            raiting_result_word = word_raiting
+            result_word = word
+    return result_word
