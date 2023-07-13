73b761a40f433b35c90f258f072acacf62276b25
Ireal <pesotskygoscha@yandex.ru>
2023-07-13 17:33:16 +0300

    feat: Decode the Morse code

---
+++ 6KuyKatas/Decode the Morse code.py
@@ -0,0 +1,5 @@
+from preloaded import MORSE_CODE
+
+def decodeMorse(morse_code):
+    message = "".join([MORSE_CODE[i] if i in MORSE_CODE else " " for i in morse_code.split(" ")])
+    return message.replace("  ", " ").strip()
